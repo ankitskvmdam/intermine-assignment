78749844528623460cce15049d524134e394dd4e
Ankit Kumar <ankits.kvmdam@hotmail.com>
2021-05-29 12:27:52 +0530

    feat: add header

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+export const Header = () => {
+    return(
+        <nav className='nav'>
+            <div className='wrapper nav-wrapper'>
+                <span className='nav-title'>
+                    InterMine Model Tree Browser
+                </span>
+            </div>
+        </nav>
+    )
+}
